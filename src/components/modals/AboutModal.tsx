e4f26089a5c3f5c08c67424a8a855a0a9802fdbd
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 08:37:02 -0700

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,13 +9,19 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+         this is an opinionated fork of an open source source clone of the game wordle -{' '}
         <a
-          href="https://github.com/cwackerfuss/react-wordle"
+          href="https://github.com/ericdrgn/wordle"
           className="underline font-bold"
         >
           check out the code here
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://www.powerlanguage.co.uk/wordle/"
+          className="underline font-bold"
+        >
+          play the original here
         </a>{' '}
       </p>
     </BaseModal>
