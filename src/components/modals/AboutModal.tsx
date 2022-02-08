4947e6e2d3fd62785037bfaf958974b45788fe4d
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 09:36:05 -0700

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="about" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
+      <p className="text-sm text-gray-500 dark:bg-black text-white">
          this is an opinionated fork of an open source source clone of the game wordle -{' '}
         <a
           href="https://github.com/ericdrgn/wordle"
