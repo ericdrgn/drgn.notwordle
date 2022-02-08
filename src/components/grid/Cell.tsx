6f510a3ce30215e2ed1d5eed91d702ecf3c75ee4
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 12:37:30 -0700

    Update Cell.tsx

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -29,9 +29,9 @@ export const Cell = ({
       'border-black dark:border-slate-100': value && !status,
       'absent shadowed bg-black border-black text-slate-500 dark:bg-black text-white dark:border-black':
         status === 'absent',
-      'correct shadowed bg-black text-green-500 border-black dark:bg-green-500 dark:text-white dark:border-white':
+      'correct shadowed bg-black text-green-500 active:text-white border-black dark:bg-green-500 dark:text-white dark:border-white':
         status === 'correct',
-      'present shadowed bg-black text-yellow-500 border-black dark:bg-yellow-500 dark:text-white dark:border-white':
+      'present shadowed bg-black text-yellow-500 border-black active:text-white dark:bg-yellow-500 dark:text-white dark:border-white':
         status === 'present',
       'cell-fill-animation': isFilled,
       'cell-reveal': shouldReveal,
