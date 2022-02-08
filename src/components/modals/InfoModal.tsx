b60cf732a4e6adab602c1a8370386d139276ed94
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 12:47:47 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="how to play" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
+      <p className="text-sm text-white dark:text-white">
         guess the word in 6 tries. after each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
@@ -21,7 +21,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="R" />
         <Cell value="Y" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
+      <p className="text-sm text-white dark:text-white">
         the letter W is in the word and in the correct spot
       </p>
 
@@ -32,7 +32,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="O" />
         <Cell value="T" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
+      <p className="text-sm text-white dark:text-white">
         the letter L is in the word but in the wrong spot
       </p>
 
@@ -43,7 +43,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="U" status="absent" />
         <Cell value="E" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
+      <p className="text-sm text-white dark:text-white">
         the letter U is not in the word in any spot
       </p>
     </BaseModal>
