25f81e64e8b97821291610f3a1516b99b2f79733
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 14:59:21 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -56,7 +56,7 @@ export const StatsModal = ({
       {(isGameLost || isGameWon) && (
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
           <div>
-            <h5>{NEW_WORD_TEXT} <a href="https://www.powerlanguage.co.uk/wordle/">original</a> </h5>
+            <h5>{NEW_WORD_TEXT} <a href="https://www.powerlanguage.co.uk/wordle/" className="underline font-bold">original</a> </h5>
             <Countdown
               className="text-lg font-medium text-white dark:text-white"
               date={tomorrow}
