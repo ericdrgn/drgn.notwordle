1d061e7fba37063d53104ab34b1436c940a34e32
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 12:29:27 -0700

    Update Cell.tsx

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -24,7 +24,7 @@ export const Cell = ({
   const classes = classnames(
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
-      'bg-white border-black dark:bg-black dark:border-white':
+      'bg-black border-black dark:bg-black dark:border-white':
         !status,
       'border-black dark:border-slate-100': value && !status,
       'absent shadowed bg-slate-400 dark:bg-black text-white border-slate-400 dark:border-black':
