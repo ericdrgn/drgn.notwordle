6e31f13cc8cd790a0b1cc363f04268f56323c59e
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-08 08:51:32 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -6,7 +6,7 @@ export const ABOUT_GAME_MESSAGE = 'about this game'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'not enough letters, loser'
 export const WORD_NOT_FOUND_MESSAGE = 'word not found, derp'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `the word was ${solution}`
 export const ENTER_TEXT = 'enter'
 export const DELETE_TEXT = 'delete'
 export const STATISTICS_TITLE = 'statistics'
