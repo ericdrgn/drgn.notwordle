0ff7c963557caf7264bab5bb87768621b616af0e
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-09 11:12:30 -0700

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -11,7 +11,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-white dark:bg-black dark:text-white">
          this is an opinionated fork of an open source source clone of the game wordle -{' '}
         <a
-          href="https://github.com/ericdrgn/wordle"
+          href="https://github.com/ericdrgn/drgn.notwordle"
           className="underline font-bold"
         >
           check out the code here
