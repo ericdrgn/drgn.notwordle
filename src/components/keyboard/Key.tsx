666aeafaf919dbde90692b65223e75563d1f4b20
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-09 09:01:02 -0700

    Update Key.tsx

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -23,15 +23,15 @@ export const Key = ({
   const keyDelayMs = REVEAL_TIME_MS * MAX_WORD_LENGTH
 
   const classes = classnames(
-    'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white',
+    'flex items-center justify-center rounded mx-0.5 text-s font-bold cursor-pointer select-none dark:text-white',
     {
       'transition ease-in-out': isRevealing,
       'bg-black text-white dark:bg-black dark:border-solid dark:border-2 dark:border-white hover:bg-black active:bg-black':
         !status,
       'bg-black text-black dark:bg-black dark:text-white': status === 'absent',
-      'bg-black text-green-500 hover:bg-black dark:bg-green-500 dark:text-white dark:border-solid dark:border-2 dark:border-green-500 dark:text-white active:bg-black':
+      'bg-black text-green-500 hover:bg-black dark:bg-green-500 dark:text-white dark:border-solid dark:border-2 dark:border-white dark:text-white active:bg-black':
         status === 'correct',
-      'bg-black text-yellow-500 hover:bg-black dark:bg-yellow-500 dark:text-white dark:border-solid dark:border-2 dark:border-yellow-500 active:bg-black':
+      'bg-black text-yellow-500 hover:bg-black dark:bg-yellow-500 dark:text-white dark:border-dashed dark:border-2 dark:border-white active:bg-black':
         status === 'present',
     }
   )
