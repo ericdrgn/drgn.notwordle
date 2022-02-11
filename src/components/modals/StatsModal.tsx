74306af920b925f9b4dd45da8d192750ede44b73
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-11 10:39:24 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -5,6 +5,7 @@ import { GameStats } from '../../lib/localStorage'
 import { shareStatus } from '../../lib/share'
 import { tomorrow } from '../../lib/words'
 import { BaseModal } from './BaseModal'
+import { solution } from '../../lib/words'
 import {
   STATISTICS_TITLE,
   GUESS_DISTRIBUTION_TEXT,
@@ -58,7 +59,7 @@ export const StatsModal = ({
       {(isGameLost || isGameWon) && (
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
           <div>
-            <h5>{NEW_WORD_TEXT} <a href="https://wordler.space" className="underline font-bold">more wordle</a> </h5>
+            <h5><a href="https://wordler.space" className="underline font-bold">return</a> {NEW_WORD_TEXT} <a href={`https://www.merriam-webster.com/dictionary/${solution}`} target="_blank" rel="noreferrer" className="underline font-bold">definition</a></h5>
             <Countdown
               className="text-lg font-medium text-white dark:text-white"
               date={tomorrow}
