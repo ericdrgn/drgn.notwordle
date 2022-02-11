b5638661372aa47e082c335425de3fe2d97d901d
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-11 14:29:04 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,19 @@
 export const GAME_TITLE = 'drudgele'
 
-export const WIN_MESSAGES = ['echo the truest hackerman', 'echo is hacking in your blood?', 'echo']
+export const WIN_MESSAGES = ['echo the truest hackerman', 'echo is hacking in your blood?', 'root@drudgle:~$']
 export const GAME_COPIED_MESSAGE = 'xclip -sel c'
 export const ABOUT_GAME_MESSAGE = 'source'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'try harder'
-export const WORD_NOT_FOUND_MESSAGE = 'word not found, derp'
+export const WORD_NOT_FOUND_MESSAGE = 'permission denied'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `echo ${solution}`
+  `achieve root with ${solution}`
 export const ENTER_TEXT = 'enter'
 export const DELETE_TEXT = 'delete'
-export const STATISTICS_TITLE = 'statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'guess distribution'
+export const STATISTICS_TITLE = 'times achieved root'
+export const GUESS_DISTRIBUTION_TEXT = 'hacked successes'
 export const NEW_WORD_TEXT = 'or'
-export const SHARE_TEXT = 'share'
-export const TOTAL_TRIES_TEXT = 'total tries'
-export const SUCCESS_RATE_TEXT = 'success rate'
-export const CURRENT_STREAK_TEXT = 'current streak'
+export const SHARE_TEXT = 'xclip'
+export const TOTAL_TRIES_TEXT = 'attempts'
+export const SUCCESS_RATE_TEXT = 'hack rate'
+export const CURRENT_STREAK_TEXT = 'hack streak'
 export const BEST_STREAK_TEXT = 'best streak'
