cade17a3c0576b316b25d60754b70af53379ae6d
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-11 13:39:40 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,43 +8,46 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="how to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="man drudgele" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-white dark:text-white">
         guess the word in 6 tries. after each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
-        <Cell value="A" />
+        <Cell value="D" status="correct" />
         <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-white dark:text-white">
-        the letter W is in the word and in the correct spot
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-white dark:text-white">
-        the letter L is in the word but in the wrong spot
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
         <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="O" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-white dark:text-white">
-        the letter U is not in the word in any spot
+        the letter D is in the password and in the correct spot
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="H" />
+        <Cell value="A" />
+        <Cell value="C" />
+        <Cell value="K" />
+        <Cell value="3" status="present" />
+        <Cell value="R" />
+      </div>
+      <p className="text-sm text-white dark:text-white">
+        the number 3 is in the password but in the wrong spot
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="T" status="absent" />
+        <Cell value="3" />
+        <Cell value="S" />
+        <Cell value="T" status="absent" />
+        <Cell value="E" />
+        <Cell value="R" />
+      </div>
+      <p className="text-sm text-white dark:text-white">
+        the letter T is not in the password in any spot
       </p>
     </BaseModal>
   )
