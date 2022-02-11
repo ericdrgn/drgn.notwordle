d5dcb89d82badadb10db1d4af254a15b70e1915b
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-11 13:35:30 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,12 +1,12 @@
-export const GAME_TITLE = 'not wordle'
+export const GAME_TITLE = 'drudgele'
 
-export const WIN_MESSAGES = ['are you... the one?', 'siiiiiiick', 'is this your world? do we just live in it?']
-export const GAME_COPIED_MESSAGE = 'game copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'about this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'not enough letters, loser'
+export const WIN_MESSAGES = ['echo the truest hackerman', 'echo is hacking in your blood?', 'echo']
+export const GAME_COPIED_MESSAGE = 'xclip -sel c'
+export const ABOUT_GAME_MESSAGE = 'source'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'try harder'
 export const WORD_NOT_FOUND_MESSAGE = 'word not found, derp'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `the word was ${solution}`
+  `echo ${solution}`
 export const ENTER_TEXT = 'enter'
 export const DELETE_TEXT = 'delete'
 export const STATISTICS_TITLE = 'statistics'
