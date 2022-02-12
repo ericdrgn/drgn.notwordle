be2965b2a4c5d37c3bfb1515d8bb9e3b8b07e940
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-11 23:28:18 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,10 +1,10 @@
 export const GAME_TITLE = 'not wordle'
 
-export const WIN_MESSAGES = ['are you... the one?', 'siiiiiiick', 'is this your world? do we just live in it?']
+export const WIN_MESSAGES = ['are you... the one?', 'not wordle, meet your maker', 'is this your world? do we just live in it?']
 export const GAME_COPIED_MESSAGE = 'game copied to clipboard'
 export const ABOUT_GAME_MESSAGE = 'about this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'not enough letters, loser'
-export const WORD_NOT_FOUND_MESSAGE = 'word not found, derp'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'not enough letters'
+export const WORD_NOT_FOUND_MESSAGE = 'unknown word'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `the word was ${solution}`
 export const ENTER_TEXT = 'enter'
@@ -12,7 +12,7 @@ export const DELETE_TEXT = 'delete'
 export const STATISTICS_TITLE = 'statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'winning rows'
 export const NEW_WORD_TEXT = 'or'
-export const SHARE_TEXT = 'share'
+export const SHARE_TEXT = 'copy'
 export const TOTAL_TRIES_TEXT = 'total tries'
 export const SUCCESS_RATE_TEXT = 'success rate'
 export const CURRENT_STREAK_TEXT = 'current streak'
