aa745320e93dd20b9fba89fbb96cbabd07d9d208
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-11 23:13:19 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -10,7 +10,7 @@ export const CORRECT_WORD_MESSAGE = (solution: string) =>
 export const ENTER_TEXT = 'enter'
 export const DELETE_TEXT = 'delete'
 export const STATISTICS_TITLE = 'statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'guess distribution'
+export const GUESS_DISTRIBUTION_TEXT = 'winning rows'
 export const NEW_WORD_TEXT = 'or'
 export const SHARE_TEXT = 'share'
 export const TOTAL_TRIES_TEXT = 'total tries'
