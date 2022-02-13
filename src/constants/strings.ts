7fb1e07fe65093f84888ec680a8a1bcda441c4f0
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-12 22:30:07 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,16 +1,16 @@
 export const GAME_TITLE = 'recurrle'
 
 export const WIN_MESSAGES = ['are you... the one?', 'nice', 'is this your world? do we just live in it?']
-export const GAME_COPIED_MESSAGE = 'game copied to clipboard'
+export const GAME_COPIED_MESSAGE = 'grid copied to clipboard'
 export const ABOUT_GAME_MESSAGE = 'about'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'wrong letter'
-export const WORD_NOT_FOUND_MESSAGE = 'letter not found'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'try harder'
+export const WORD_NOT_FOUND_MESSAGE = 'unknown word'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `the letter was ${solution}`
 export const ENTER_TEXT = 'enter'
 export const DELETE_TEXT = 'delete'
 export const STATISTICS_TITLE = 'statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'winning rows'
+export const GUESS_DISTRIBUTION_TEXT = ''
 export const NEW_WORD_TEXT = 'or'
 export const SHARE_TEXT = 'copy'
 export const TOTAL_TRIES_TEXT = 'attempts'
