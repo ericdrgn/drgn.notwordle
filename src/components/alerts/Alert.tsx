a3705da48f118d11071f50490a5d27b76ebeeea2
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-12 23:19:19 -0700

    Update Alert.tsx

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -13,7 +13,7 @@ export const Alert = ({ isOpen, message, variant = 'warning', topMost = true }: 
   const classes = classNames(
     'fixed top-5 left-1/2 transform -translate-x-1/2 max-w-sm w-full shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden',
     {
-      'bg-red-500 text-white': variant === 'warning',
+      'bg-slate-800 text-white': variant === 'warning',
       'bg-black text-white': variant === 'success',
     },
     topMost ? 'z-20' : ''
