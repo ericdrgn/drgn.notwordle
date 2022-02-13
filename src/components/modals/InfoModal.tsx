b93ff37bab0ffce66b1836a4896effd954c826d7
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-12 22:12:15 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -15,23 +15,23 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="D" status="correct" />
-        <Cell value="R" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="O" />
-        <Cell value="N" />
+        <Cell value="R" status="absent" />
+        <Cell value="A" status="absent" />
+        <Cell value="G" status="absent" />
+        <Cell value="O" status="absent" />
+        <Cell value="N" status="absent" />
       </div>
       <p className="text-sm text-white dark:text-white">
         the letter D is in the password and in the correct spot
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="H" />
-        <Cell value="A" />
-        <Cell value="C" />
-        <Cell value="K" />
+        <Cell value="H" status="absent" />
+        <Cell value="A" status="absent" />
+        <Cell value="C" status="absent" />
+        <Cell value="K" status="absent" />
         <Cell value="3" status="present" />
-        <Cell value="R" />
+        <Cell value="R" status="absent" />
       </div>
       <p className="text-sm text-white dark:text-white">
         the number 3 is in the password but in the wrong spot
@@ -39,11 +39,11 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="T" status="absent" />
-        <Cell value="3" />
-        <Cell value="S" />
+        <Cell value="3" status="correct" />
+        <Cell value="S" status="correct" />
         <Cell value="T" status="absent" />
-        <Cell value="E" />
-        <Cell value="R" />
+        <Cell value="E" status="correct" />
+        <Cell value="R" status="correct" />
       </div>
       <p className="text-sm text-white dark:text-white">
         the letter T is not in the password in any spot
