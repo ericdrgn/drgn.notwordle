401d52cafdabd1ea48e8bd72b2e0059b109b4294
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-13 00:46:27 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {
   QuestionMarkCircleIcon,
-  ViewListIcon,
+  TableIcon,
   EyeOffIcon,
   EyeIcon,
   ViewGridAddIcon,
@@ -249,7 +249,7 @@ function App() {
             onClick={() => handleDarkMode(!isDarkMode)}
           />
         )}
-        <ViewListIcon
+        <TableIcon
           className="h-6 w-6 mr-3 cursor-pointer stroke-slate-800 dark:stroke-white"
           onClick={() => setIsStatsModalOpen(true)}
         />
