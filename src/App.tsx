f76091927255c35c380228f398249f46083ec0ff
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-13 00:57:25 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {
   QuestionMarkCircleIcon,
-  ChartBarIcon,
+  UserGroupIcon,
   EyeOffIcon,
   EyeIcon,
   ViewGridIcon,
@@ -249,7 +249,7 @@ function App() {
             onClick={() => handleDarkMode(!isDarkMode)}
           />
         )}
-        <ChartBarIcon
+        <UserGroupIcon
           className="h-6 w-6 mr-3 cursor-pointer stroke-slate-800 dark:stroke-white"
           onClick={() => setIsStatsModalOpen(true)}
         />
