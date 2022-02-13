b05c487df76111295fd99ae212a9e7b00bff349b
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-12 22:50:44 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -1,13 +1,11 @@
 import { Cell } from '../grid/Cell'
 import { BaseModal } from './BaseModal'
-import { AboutModal } from './AboutModal'
 
 type Props = {
   isOpen: boolean
   handleClose: () => void
 }
 
-const [isAboutModalOpen, setIsAboutModalOpen] = useState(false)
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="how to play" isOpen={isOpen} handleClose={handleClose}>
@@ -28,19 +26,6 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-white dark:text-white">
         U is not the letter
       </p>
-      
-      <AboutModal
-        isOpen={isAboutModalOpen}
-        handleClose={() => setIsAboutModalOpen(false)}
-      />
-      
-      <button
-        type="button"
-        className="mx-auto mt-8 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-black dark:text-white bg-black hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black select-none"
-        onClick={() => setIsAboutModalOpen(true)}
-      >
-        {ABOUT_GAME_MESSAGE}
-      </button>
     </BaseModal>
   )
 }
