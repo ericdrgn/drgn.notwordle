0cc5d919e0be4b2bdc1fa30a5c76d1fe922e4014
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-12 21:35:59 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,18 +2,18 @@ export const GAME_TITLE = 'recurrle'
 
 export const WIN_MESSAGES = ['are you... the one?', 'nice', 'is this your world? do we just live in it?']
 export const GAME_COPIED_MESSAGE = 'game copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'about this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'unknown word'
+export const ABOUT_GAME_MESSAGE = 'about'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'wrong letter'
+export const WORD_NOT_FOUND_MESSAGE = 'letter not found'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `the word was ${solution}`
+  `the letter was ${solution}`
 export const ENTER_TEXT = 'enter'
 export const DELETE_TEXT = 'delete'
 export const STATISTICS_TITLE = 'statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'winning rows'
 export const NEW_WORD_TEXT = 'or'
 export const SHARE_TEXT = 'copy'
-export const TOTAL_TRIES_TEXT = 'total tries'
-export const SUCCESS_RATE_TEXT = 'success rate'
-export const CURRENT_STREAK_TEXT = 'current streak'
+export const TOTAL_TRIES_TEXT = 'attempts'
+export const SUCCESS_RATE_TEXT = 'win rate'
+export const CURRENT_STREAK_TEXT = 'streak'
 export const BEST_STREAK_TEXT = 'best streak'
