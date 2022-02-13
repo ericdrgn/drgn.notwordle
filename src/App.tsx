6c97d2b24f004eed5746510d0b25ae5271698a13
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-13 00:53:30 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {
   QuestionMarkCircleIcon,
-  MenuAlt3Icon,
+  MenuIcon,
   EyeOffIcon,
   EyeIcon,
   ViewGridAddIcon,
@@ -249,7 +249,7 @@ function App() {
             onClick={() => handleDarkMode(!isDarkMode)}
           />
         )}
-        <MenuAlt3Icon
+        <MenuIcon
           className="h-6 w-6 mr-3 cursor-pointer stroke-slate-800 dark:stroke-white"
           onClick={() => setIsStatsModalOpen(true)}
         />
