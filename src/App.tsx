50f52b6750a1b87e62c8adfcfafda553789ba7bd
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-14 14:04:36 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -92,6 +92,14 @@ function App() {
   const [isMissingPreviousLetters, setIsMissingPreviousLetters] =
     useState(false)
   const [missingLetterMessage, setIsMissingLetterMessage] = useState('')
+  
+  useEffect(() => {
+    // if no game state on load,
+    // show the user the how-to info modal
+    if (!loadGameStateFromLocalStorage()) {
+      setIsInfoModalOpen(true)
+    }
+  }, [])
 
   useEffect(() => {
     if (isDarkMode) {
