e85eddabf2e33bcb452d84f710366167db0e8c0c
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-13 22:12:31 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="how to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-white dark:text-white">
-        guess the word in 6 tries. after each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        you will have six guesses to get the wordle of the day. when a word is guessed
+        the colors of the letters will change to reflect positions for the correct word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -22,22 +22,23 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="E" status="absent" />
       </div>
       <p className="text-sm text-white dark:text-white">
-        the letter F is perfectly placed, letters L, M, and E are not in the word, and the letter A is in the word in the wrong spot
+        the letter F is perfectly placed, letters L, M, and E are not in the word, and the letter A is in the word, but is in the wrong spot
       </p>
+      
       <p className="text-sm text-white dark:bg-black dark:text-white">
-         this is an opinionated fork of an open source source clone of the game wordle -{' '}
+         \n this is an opinionated fork of an open source source clone of the game wordle -{' '}
         <a
           href="https://github.com/ericdrgn/drgn.notwordle"
           className="underline font-bold"
         >
-          check out the code here
+          source
         </a>{' '}
         and{' '}
         <a
           href="https://wordler.space"
           className="underline font-bold"
         >
-          play other versions here
+          other versions
         </a>{' '}
       </p>
     </BaseModal>
