8154a3a1085779bcb57d29e1d374a52147296111
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-16 20:30:26 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,5 +1,5 @@
 export const GAME_TITLE = 'drudgele'
-export const WIN_MESSAGES = ['echo the truest hackerman', 'echo is hacking in your blood?', 'root@drudgle:~$']
+export const WIN_MESSAGES = ['solid', 'nice', 'root@drudgle:~$']
 export const GAME_COPIED_MESSAGE = 'xclip -sel c'
 export const ABOUT_GAME_MESSAGE = 'source'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'try harder'
@@ -16,10 +16,10 @@ export const TOTAL_TRIES_TEXT = 'attempts'
 export const SUCCESS_RATE_TEXT = 'hack rate'
 export const CURRENT_STREAK_TEXT = 'hack streak'
 export const BEST_STREAK_TEXT = 'best streak'
-export const HARD_MODE = 'now in god mode'
-export const EASY_MODE = 'now in n00b mode'
-export const FOCUS_MODE = 'now using hacker interface'
-export const VIS_MODE = 'now using n00b interface'
+export const HARD_MODE = 'god mode'
+export const EASY_MODE = 'n00b mode'
+export const FOCUS_MODE = 'hacker interface'
+export const VIS_MODE = 'n00b interface'
 export const HARD_MODE_ALERT_MESSAGE =
   'enable before first guess'
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
