fad52c16959d9948499487c885e93efa7a69566d
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-18 12:44:04 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -61,13 +61,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
                   />
       </div>
       <p className="text-sm text-white dark:bg-black dark:text-white">
-        <a
-          href="https://github.com/ericdrgn/drgn.notwordle/tree/extremele"
-          className="underline font-bold"
-        >
-          source
-        </a>{' '}
-        and{' '}
+        check out{' '}
         <a
           href="https://wordler.space"
           className="underline font-bold"
