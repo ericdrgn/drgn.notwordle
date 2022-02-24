d4e2e21891d87d137f64af9d6a99122a6c451381
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-23 19:53:31 -0700

    Update Alert.tsx

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -11,10 +11,10 @@ type Props = {
 
 export const Alert = ({ isOpen, message, variant = 'error', topMost = true }: Props) => {
   const classes = classNames(
-    'flex z-20 max-w-sm',
+    'flex z-2000 max-w-sm',
     {
-      'bg-black text-white z-20': variant === 'error',
-      'bg-transparent text-white z-20': variant === 'success',
+      'bg-black text-white': variant === 'error',
+      'bg-transparent text-white': variant === 'success',
     }
   )
 
