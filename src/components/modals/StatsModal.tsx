2304b095da843c51adf281551cc4d33ead4fd242
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-23 18:56:10 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -63,12 +63,12 @@ export const StatsModal = ({
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
           <div>
             <h5><a href="https://wordler.space" className="underline font-bold">return</a> {NEW_WORD_TEXT} <a href={`https://en.wiktionary.org/wiki/${solutionDefinition}`} target="_blank" rel="noreferrer" className="underline font-bold">definition</a></h5>
+            <AlertContainer />
             <Countdown
               className="text-lg font-medium text-white dark:text-white"
               date={tomorrow}
               daysInHours={true}
             />
-          <AlertContainer />
           </div>
           <button
             type="button"
