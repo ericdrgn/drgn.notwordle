d03a19684fbf713b99037d8f1b94f7843a52785d
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2022-02-23 17:34:13 -0700

    Update BaseModal.tsx

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -47,8 +47,8 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
+            <AlertContainer />
             <div className="inline-block align-bottom bg-black rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 dark:bg-black text-white">
-              <AlertContainer />
               <div className="absolute right-4 top-4">
                 <XCircleIcon
                   className="h-6 w-6 cursor-pointer dark:stroke-white"
@@ -62,6 +62,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
                     className="text-lg leading-6 font-medium dark:text-white"
                   >
                     {title}
+                    <AlertContainer />
                   </Dialog.Title>
                   <div className="mt-2">{children}</div>
                 </div>
