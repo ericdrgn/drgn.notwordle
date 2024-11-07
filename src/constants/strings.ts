e67a779e92a312fd5254d64daf235854c3f194a6
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2024-11-07 10:58:06 -0700

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,28 +1,28 @@
-export const GAME_TITLE = 'recurrle'
-export const WIN_MESSAGES = ['are you, the one?', 'nice', 'is this your world?']
-export const GAME_COPIED_MESSAGE = 'grid copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'about'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'try harder'
-export const WORD_NOT_FOUND_MESSAGE = 'unknown word'
+export const GAME_TITLE = 'Recurrle'
+export const WIN_MESSAGES = ['Are you, the one?', 'Nice', 'Is this your world?']
+export const GAME_COPIED_MESSAGE = 'Grid copied to clipboard'
+export const ABOUT_GAME_MESSAGE = 'About'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Try harder'
+export const WORD_NOT_FOUND_MESSAGE = 'Unknown word'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `the word was ${solution}`
-export const ENTER_TEXT = 'enter'
-export const DELETE_TEXT = 'delete'
-export const STATISTICS_TITLE = 'statistics'
+  `The word was ${solution}`
+export const ENTER_TEXT = 'Enter'
+export const DELETE_TEXT = 'Delete'
+export const STATISTICS_TITLE = 'Statistics'
 export const GUESS_DISTRIBUTION_TEXT = ''
-export const NEW_WORD_TEXT = 'or'
-export const SHARE_TEXT = 'copy'
-export const TOTAL_TRIES_TEXT = 'attempts'
-export const SUCCESS_RATE_TEXT = 'win rate'
-export const CURRENT_STREAK_TEXT = 'streak'
-export const BEST_STREAK_TEXT = 'best streak'
-export const HARD_MODE = 'now in hard mode'
-export const EASY_MODE = 'now in easy mode'
-export const FOCUS_MODE = 'now using minimal interface'
-export const VIS_MODE = 'now using standard interface'
+export const NEW_WORD_TEXT = 'Or'
+export const SHARE_TEXT = 'Copy'
+export const TOTAL_TRIES_TEXT = 'Attempts'
+export const SUCCESS_RATE_TEXT = 'Win rate'
+export const CURRENT_STREAK_TEXT = 'Streak'
+export const BEST_STREAK_TEXT = 'Best streak'
+export const HARD_MODE = 'Now in hard mode'
+export const EASY_MODE = 'Now in easy mode'
+export const FOCUS_MODE = 'Now using minimal interface'
+export const VIS_MODE = 'Now using standard interface'
 export const HARD_MODE_ALERT_MESSAGE =
-  'enable before first guess'
+  'Enable before first guess'
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `must use ${guess} in position ${position}`
+  `Must use ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `guess must contain ${letter}`
+  `Guess must contain ${letter}`
