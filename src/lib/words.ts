71f4e1c769f7a838e974b2c3c73fd6a6c064b9fe
Eric Dragon <60805441+ericdrgn@users.noreply.github.com>
2024-11-07 11:24:29 -0700

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -53,7 +53,7 @@ export const findFirstUnusedReveal = (word: string, guesses: string[]) => {
 
 export const getWordOfDay = () => {
   // specific epoch to keep all words the same just based on every 30 minutes
-  const epochMs = 1644562800000
+  const epochMs = 1730937600
   const now = Date.now()
   const msInDay = 3600000
   const index = Math.floor((now - epochMs) / msInDay)
